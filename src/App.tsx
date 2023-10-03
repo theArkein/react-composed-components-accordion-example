116b74046b77f3489de6a9e8144efd21dd1569ba
Sarad Singh <theArkein.dev@gmail.com>
2023-10-03 21:09:17 +0545

    Add description

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import Accordion from 'headless-components/Accordion';
 
 export default function App() {
    return (
-      <>
+      <div>
+         <h1>Composition Pattern</h1>
+         <p>
+            Breaking components down into smaller, reusable parts that can be composed together to
+            form more complex UIs
+         </p>
+         <ul>
+            Advantages of composition pattern
+            <li>Redueced props in the Main component</li>
+            <li>Explicit Control of props by sub components</li>
+            <li>Clean Structure</li>
+            <li>Maintainable</li>
+         </ul>
          <Accordion>
             <Accordion.Title className="p-4 bg-zinc-200 font-semibold cursor-pointer">
                Item 1
@@ -13,6 +25,6 @@ export default function App() {
                et fugiat? Assumenda tenetur necessitatibus facilis nisi?
             </Accordion.Body>
          </Accordion>
-      </>
+      </div>
    );
 }
